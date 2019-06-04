9ab3272b30b296ca4f607686db46a321f9ec2915
Mufaddal Jerwalla <Mufaddal@Muffy-8.local>
2019-06-04 12:28:07 -0400

    Adding elementary application

---
+++ photofeed/feed/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import JsonResponse
+from .forms import * 
+from pusher import Pusher 
+import json 
+
+pusher = Pusher(app_id=u'796493', key=u'627edbfb8e96c901960e', secret=u'b2274d773cc6e5c4b25b', cluster=u'us2')
+
+def index(request):
+	all_documents=Feed.objects.all().order_by('-id')
+	return render(request, 'index.html', {'all_documents': all_documents})
+# Create your views here.
+
+def pusher_authentication(request):
+	channel = request.GET.get('channel_name', None)
+	socket_id = request.GET.get('socket_id', None)
+	auth = pusher.authenticate(
+		channel = channel, 
+		socket_id = socket_id
+	)
+	return JsonResponse(json.dumps(auth), safe=False)
+
+def push_feed(request):
+	if request.method == 'POST': 
+		form = DocumentForm(request.POST, request.FILES)
+		if form.is_valid():
+			f = form.save()
+			pusher.trigger(u'a_channel',u'an_event', {u'description': f.description, u'document': f.document.url})
+			return HttpResponse('ok')
+		else:
+			return HttpResponse('form not valid')
+	else: 
+		return HttpResponse('error, please try again')
+
+def delete_all(request):
+	print("In delete all")
+	if request.method == "POST":
+		all_documents = Feed.objects.all().delete()
+	return HttpResponse('delete completed')
+
+
+def delete_one(request):
+	print("In delete one")
+	print(request.POST['id'])
+	# print(data)
+	if request.method == "POST":
+		all_documents = Feed.objects.filter(description=request.POST['id']).delete()
+	return HttpResponse('delete completed')
+
+
+def share_pic(request, pic_id):
+	print("HERE")
+	print(pic_id)
+	doc = get_object_or_404(Feed, description=pic_id)
+	return render(request, 'trial.html', {'all_documents': doc})
+
+def get_share_url(request, pic_id): 
+	#Generate shareable links, hardcoding for now 
+	doc = 'localhost:8080/share_pic/'
+	print(pic_id)
+	return HttpResponse(doc)
+	# return JsonResponse({'res':doc})
---
+++ photofeed/photofeed/urls.py
@@ -0,0 +1,29 @@
+"""photofeed URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from feed.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('push_feed', push_feed), 
+    path('share_pic/<slug:pic_id>', share_pic),
+    path('get_share/<slug:pic_id>', get_share_url),
+    path('delete_all', delete_all),
+    path('delete_one', delete_one),
+    path('pusher_authentication', pusher_authentication)
+]
